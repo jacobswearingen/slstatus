ffcc9ebae2c492411f92e06a89a9093dfdc23616
Jacob Swearingen <flobbus@gmail.com>
2026-04-03 14:22:08 -0500

    claude: handle waiting for pipewire to start

--- components/volume.c
+++ components/volume.c
@@ -1,73 +1,88 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 #include <pulse/pulseaudio.h>
 
-static char vol_str[8] = {'\0'};
+static char vol_str[8] = "--";
 static int vol_muted = 0;
-
-static pa_mainloop *mainloop = NULL;
 static int data_ready = 0;
 
-void context_state_cb(pa_context *c, void *userdata);
-void sink_info_cb(pa_context *c, const pa_sink_info *i, int eol, void *userdata);
+static void context_state_cb(pa_context *c, void *userdata);
+static void sink_info_cb(pa_context *c, const pa_sink_info *i, int eol,
+                         void *userdata);
 
-void prepare_data(const char *card) {
+static int prepare_data(const char *card)
+{
+  pa_mainloop *ml;
+  pa_context *ctx;
+  int ret;
+
   data_ready = 0;
 
-  mainloop = pa_mainloop_new();
-  pa_mainloop_api *mainloop_api = pa_mainloop_get_api(mainloop);
-  pa_context *context = pa_context_new(mainloop_api, "get_sink_information");
+  ml = pa_mainloop_new();
+  if (!ml)
+    return -1;
 
-  pa_context_set_state_callback(context, context_state_cb, (void *)card);
-  pa_context_connect(context, NULL, PA_CONTEXT_NOFLAGS, NULL);
-
-  while (!data_ready) {
-    pa_mainloop_iterate(mainloop, 1, NULL);
+  ctx = pa_context_new(pa_mainloop_get_api(ml), "slstatus");
+  if (!ctx) {
+    pa_mainloop_free(ml);
+    return -1;
   }
 
-  pa_context_disconnect(context);
-  pa_context_unref(context);
-  pa_mainloop_free(mainloop);
+  pa_context_set_state_callback(ctx, context_state_cb, (void *)card);
+  if (pa_context_connect(ctx, NULL, PA_CONTEXT_NOFLAGS, NULL) < 0) {
+    pa_context_unref(ctx);
+    pa_mainloop_free(ml);
+    return -1;
+  }
+
+  /* run the mainloop until data_ready is set or an error occurs */
+  while (!data_ready) {
+    if (pa_mainloop_iterate(ml, 1, &ret) < 0)
+      break;
+  }
+
+  pa_context_disconnect(ctx);
+  pa_context_unref(ctx);
+  pa_mainloop_free(ml);
+
+  return data_ready ? 0 : -1;
 }
 
 const char *vol_perc(const char *card) {
-  prepare_data(card);
+  if (prepare_data(card) < 0)
+    return NULL;
   return vol_str;
 }
 
 const char *vol_mute(const char *card) {
-  prepare_data(card);
-  static char key[16] = {'\0'};
-  snprintf(key, sizeof(key), "%s", vol_muted ? "MUT" : "VOL");
-  return key;
+  if (prepare_data(card) < 0)
+    return NULL;
+  return vol_muted ? "MUT" : "VOL";
 }
 
-void context_state_cb(pa_context *c, void *userdata) {
-  const char *card_name = userdata;
+static void context_state_cb(pa_context *c, void *userdata) {
   switch (pa_context_get_state(c)) {
-    case PA_CONTEXT_READY:
-      pa_context_get_sink_info_by_name(c, card_name, sink_info_cb, NULL);
-      break;
-    case PA_CONTEXT_FAILED:
-    case PA_CONTEXT_TERMINATED:
-      data_ready = 1;
-      break;
-    default:
-      break;
+  case PA_CONTEXT_READY:
+    pa_context_get_sink_info_by_name(c, (const char *)userdata,
+                                     sink_info_cb, NULL);
+    break;
+  case PA_CONTEXT_FAILED:
+  case PA_CONTEXT_TERMINATED:
+    data_ready = 1;
+    break;
+  default:
+    break;
   }
 }
 
-void sink_info_cb(pa_context *c, const pa_sink_info *i, int eol, void *userdata) {
-  if (eol > 0) {
+static void sink_info_cb(pa_context *c, const pa_sink_info *i, int eol, void *userdata) {
+  if (eol > 0 || !i) {
     data_ready = 1;
     return;
   }
 
-  unsigned volume_percent = (unsigned)(100 * pa_cvolume_avg(&(i->volume)) / PA_VOLUME_NORM);
-  snprintf(vol_str, sizeof(vol_str), "%u", volume_percent);
-
+  snprintf(vol_str, sizeof(vol_str), "%u",
+           (unsigned)(100 * pa_cvolume_avg(&i->volume) / PA_VOLUME_NORM));
   vol_muted = i->mute;
-
   data_ready = 1;
 }
